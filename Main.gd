a76f48b579ae507940615706b4beeddd2bed9c28
Kyle Eichlin <57882701+likeich@users.noreply.github.com>
2021-08-24 18:09:55 -0400

    Added android support
    Android can now ping using it's built in shell as well. The window settings were tweaked so that it looks good on android and the font was changed.

--- Main.gd
+++ Main.gd
@@ -20,6 +20,9 @@ func _process(_delta):
 		progress.value = progress.max_value
 		ping_button.disabled = false
 
+func is_mobile() -> bool:
+	return OS.get_name() == "Android"
+
 func _on_PingButton_pressed():
 	ping_button.disabled = true
 	output.text = ""
@@ -35,8 +38,10 @@ func ping(item: IPItem) -> void:
 		var _error := OS.execute("ping", [item.get_ip(), "-c", item.get_count()], true, returned)
 	elif OS.get_name() == "Windows":
 		var _error := OS.execute("ping", [item.get_ip(), "/n", item.get_count()], true, returned)
+	elif OS.get_name() == "Android":
+		var _error := OS.execute("/system/bin/ping", ["-c", item.get_count(), item.get_ip()], true, returned)
 	else:
-		output.text += OS.get_name() + " is unsupported. Try Linux or Windows"
+		output.text += OS.get_name() + " is unsupported. Try Linux, Android, or Windows"
 	
 	# Exit Work
 	mutex.lock()
